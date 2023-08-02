a38696605f0c247ed2774b77959bb760dbffd062
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-08-02 03:23:01 +0300

    %80

--- JetLink/Controllers/TextConversion.cs
+++ JetLink/Controllers/TextConversion.cs
@@ -23,43 +23,74 @@ namespace JetLink.Controllers
             bool previousWasNumber = false;
             bool resultAdded = false;
             int result = 1;
+            int insertIndex = 0;
+            int initialResult = 1;
+            int indexCounter = 0;
+            int prevNumber;
+            
             for (int i = 0; i < words.Length; i++)
             {
-
+                
                 if (turkishNumbers.ContainsKey(words[i].ToLower()))
                 {
                     int number = turkishNumbers[words[i].ToLower()];
 
+                    
                     if (previousWasNumber)
                     {
-                        result += number;
+                        if (number == 1000)
+                        {
+                            result *= number;
+                        }
+                        else if (number==100)
+                        {
+                            prevNumber = turkishNumbers[words[i - 1].ToLower()];
+                            result = result - prevNumber;
+                            result += prevNumber * number;
+                        }
+                        else
+                        { 
+                               
+                            result += number;
+                        }
+                       
                        
                         previousWasNumber = true;
                         
                     }
                     else
                     {
-                      
                         result = number;                    
                         previousWasNumber = true;
+                        insertIndex = i;                    
                     }
-
+                   
                 }
                 else
                 {
-                    if (!resultAdded) // Check if the result has been added before
+                    if (result!=1)
                     {
-                        resultWords.Add(result.ToString());
-                        resultAdded = true; // Set the flag to true to indicate that the result has been added
-                    }
+                        if (result != initialResult)
+                        {
+                            resultAdded = false;
+                        }
+                        if (!resultAdded)
+                        {
+                            initialResult = result;
+                            //resultWords.Insert(insertIndex, result.ToString());
+                            resultWords.Insert(indexCounter, result.ToString());
+                            resultAdded = true;
+                            indexCounter++;
+                        }
+                    }              
                     resultWords.Add(words[i]);
+                    indexCounter++;
                     previousWasNumber = false;
-                }
-
-              
+                }   
             }
 
-            // Sonuç metnini birleştiriyoruz //"\"" + result.ToString() + "\" " + 
+            
+
             string resultString = string.Join(" ", resultWords);
             return resultString;
         }
@@ -68,6 +99,8 @@ namespace JetLink.Controllers
         {
             var turkishNumbers = new Dictionary<string, int>
             {
+
+                // Sadece  bin yeterli olacak !!
                 { "bin", 1000 },
                 { "iki bin", 2000 },
                 { "üç bin", 3000 },
@@ -96,7 +129,36 @@ namespace JetLink.Controllers
                 { "yetmiş", 70 },
                 { "seksen", 80 },
                 { "doksan", 90 },
-                { "yüz", 100 }
+                { "yüz", 100 },
+                { "1000", 1000 },
+                { "2000", 2000 },
+                { "3000", 3000 },
+                { "4000", 4000 },
+                { "5000", 5000 },
+                { "6000", 6000 },
+                { "7000", 7000 },
+                { "8000", 8000 },
+                { "9000", 9000 },
+                { "10000", 10000 },
+                { "1", 1 },
+                { "2", 2 },
+                { "3", 3 },
+                { "4", 4 },
+                { "5", 5 },
+                { "6", 6 },
+                { "7", 7 },
+                { "8", 8 },
+                { "9", 9 },
+                { "10", 10 },
+                { "20", 20 },
+                { "30", 30 },
+                { "40", 40 },
+                { "50", 50 },
+                { "60", 60 },
+                { "70", 70 },
+                { "80", 80 },
+                { "90", 90 },
+                { "100", 100 }
             };
 
             return turkishNumbers;
